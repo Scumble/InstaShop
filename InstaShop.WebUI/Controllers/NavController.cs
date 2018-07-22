477658810d9bb2b8433752a42edbff57ad84ea22
Scumble <namchan53@gmail.com>
2018-07-22 21:37:01 +0300

    New Navbar though categories

---
+++ InstaShop.WebUI/Controllers/NavController.cs
@@ -0,0 +1,29 @@
+﻿using InstaShop.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstaShop.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IClothesRepository repository;
+
+        public NavController(IClothesRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = repository.Items
+                .Select(item => item.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
