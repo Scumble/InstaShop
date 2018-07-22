f98b866bfbc3d3f15a2063a9afd0ab6a3eff441f
Scumble <namchan53@gmail.com>
2018-07-22 21:22:31 +0300

    Edit URL

--- InstaShop.WebUI/App_Start/RouteConfig.cs
+++ InstaShop.WebUI/App_Start/RouteConfig.cs
@@ -13,16 +13,36 @@ namespace InstaShop.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-             name: null,
-             url: "Page{page}",
-             defaults: new { controller = "Item", action = "List" }
-         );
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Item", action = "List", id = UrlParameter.Optional }
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Item",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Item", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Item", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Item", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
