6e510271e7517be70a9ef6f889e6aade595b750c
Scumble <namchan53@gmail.com>
2018-07-24 21:35:09 +0300

    View for cart

---
+++ InstaShop.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InstaShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaShop.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
