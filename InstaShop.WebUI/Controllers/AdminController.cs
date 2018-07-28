647210897f25a4aded04a396860cedb8673b1ed3
Scumble <namchan53@gmail.com>
2018-07-28 21:18:40 +0300

    Added controller for admin

---
+++ InstaShop.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using InstaShop.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstaShop.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IClothesRepository repository;
+
+        public AdminController(IClothesRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Items);
+        }
+    }
+}
