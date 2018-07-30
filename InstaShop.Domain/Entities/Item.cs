56c32be5424546bb9f66bc3280293ac62f10e308
Scumble <namchan53@gmail.com>
2018-07-30 21:23:20 +0300

    Some changes

--- InstaShop.Domain/Entities/Item.cs
+++ InstaShop.Domain/Entities/Item.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace InstaShop.Domain.Entities
 {
     public class Item
     {
+        [HiddenInput(DisplayValue = false)]
         public int ItemId { get; set; }
+        [Display(Name = "Название")]
+        [Required(ErrorMessage = "Пожалуйста, введите название товара")]
         public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Пожалуйста, введите описание для товара")]
         public string Description { get; set; }
+        [Display(Name = "Категория")]
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию для товара")]
         public string Category { get; set; }
+        [Display(Name = "Цена (руб)")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     }
 }
