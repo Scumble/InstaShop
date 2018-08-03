f41938b210cedad63b982a694e9eec9076c53586
Scumble <namchan53@gmail.com>
2018-08-03 21:46:58 +0300

    *

---
+++ InstaShop.WebUI/Infrastructure/Concrete/FormAuthProvider.cs
@@ -0,0 +1,20 @@
+﻿using InstaShop.WebUI.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace InstaShop.WebUI.Infrastructure.Concrete
+{
+    public class FormAuthProvider:IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+                FormsAuthentication.SetAuthCookie(username, false);
+            return result;
+        }
+    }
+}
